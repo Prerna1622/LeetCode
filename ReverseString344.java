d9f8aa6ed64bb282d1513abc4b8636d596d32fc5
Prerna <prernarajumane16@gmail.com>
2025-08-31 20:17:05 +0530

    change

--- ReverseString344.java
+++ ReverseString344.java
@@ -1,24 +1,22 @@
-class ReverseString344 
-{
-    public void reverseString(char[] s) 
-    {
-        int n=s.length;
-        int start=0;
-        int end=n-1;
-        while(start<=end)
-        {
-            char temp=s[start];
-            s[start]=s[end];
-            s[end]=temp;
+class ReverseString344 {
+    public static void reverseString(char[] s) {
+        int n = s.length;
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
             start++;
             end--;
         }
 
-        String reversedString=String.valueOf(s);
+        String reversedString1 = String.valueOf(s);
+        System.out.println("Reversed String: " + reversedString1);
     }
-    public static void main(string args[])
-    {
-        char s[]={"h","e","l","l","o"};
+
+    public static void main(String args[]) {
+        char s[] = { 'h', 'e', 'l', 'l', 'o' };
         reverseString(s);
     }
 }
